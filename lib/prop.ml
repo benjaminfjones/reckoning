728147e834351f7fd311a6d34ec1c50be508616c
Benjamin F Jones <benjaminfjones@gmail.com>
2023-09-21 15:49:29 -0700

    Add naive truth table based DNF

--- lib/prop.ml
+++ lib/prop.ml
@@ -131,6 +131,93 @@ let satisfiable fm = not (unsatisfiable fm)
 let psubst subfn = onatoms (fun p -> tryapplyd subfn p (Atom p))
 
 (* ------------------------------------------------------------------------- *)
+(* Simplification and Normal Forms                                           *)
+(* ------------------------------------------------------------------------- *)
+
+let psimplify1 fm =
+  match fm with
+  | Not False -> True
+  | Not True -> False
+  | Not (Not p) -> p
+  | And (_p, False) | And (False, _p) -> False
+  | And (p, True) | And (True, p) -> p
+  | Or (p, False) | Or (False, p) -> p
+  | Or (_p, True) | Or (True, _p) -> True
+  | Imp (False, _p) | Imp (_p, True) -> True
+  | Imp (True, p) -> p
+  | Imp (p, False) -> Not p
+  | Iff (p, True) | Iff (True, p) -> p
+  | Iff (p, False) | Iff (False, p) -> Not p
+  | _ -> fm
+
+let rec psimplify fm =
+  match fm with
+  | Not p -> psimplify1 (Not (psimplify p))
+  | And (p, q) -> psimplify1 (And (psimplify p, psimplify q))
+  | Or (p, q) -> psimplify1 (Or (psimplify p, psimplify q))
+  | Imp (p, q) -> psimplify1 (Imp (psimplify p, psimplify q))
+  | Iff (p, q) -> psimplify1 (Iff (psimplify p, psimplify q))
+  | _ -> fm
+
+(* Is the literal negative (must be applied only to literals) *)
+let negative = function Not _ -> true | _ -> false
+let positive lit = not (negative lit)
+let negate = function Not p -> p | p -> Not p
+
+let nnf fm =
+  let rec nnf_aux fm =
+    match fm with
+    | And (p, q) -> And (nnf_aux p, nnf_aux q)
+    | Or (p, q) -> Or (nnf_aux p, nnf_aux q)
+    | Imp (p, q) -> Or (nnf_aux (Not p), nnf_aux q)
+    | Iff (p, q) ->
+        Or (And (nnf_aux p, nnf_aux q), And (nnf_aux (Not p), nnf_aux (Not q)))
+    | Not (Not p) -> nnf_aux p
+    | Not (And (p, q)) -> Or (nnf_aux (Not p), nnf_aux (Not q))
+    | Not (Or (p, q)) -> And (nnf_aux (Not p), nnf_aux (Not q))
+    | Not (Imp (p, q)) -> And (nnf_aux p, nnf_aux (Not q))
+    | Not (Iff (p, q)) ->
+        Or (And (nnf_aux p, nnf_aux (Not q)), And (nnf_aux (Not p), nnf_aux q))
+    | _ -> fm
+  in
+  nnf_aux (psimplify fm)
+
+let list_conj = function [] -> True | l -> end_itlist mk_and l
+and list_disj = function [] -> False | l -> end_itlist mk_or l
+
+(* Given a list of formulas, make a conjunction of "literals" according to
+ * whether each is satisfied by valuation `v`.
+ *
+ * This is meant to be called on a list of Atomic formulas.
+ *)
+let mk_lits pvs v =
+  list_conj (List.map (fun p -> if eval p v then p else Not p) pvs)
+
+(* Collect valuations which hold on `subfn`
+ *
+ * The formulas `fms` are meant to be atomic formulas. An example * `subfn :
+ * valuation -> bool` tha is useful is evaluation of a top-level formula using
+ * the current valuation being explored.
+ *)
+let rec allsatvaluations subfn v fms =
+  match fms with
+  | [] -> if subfn v then [ v ] else []
+  | p :: ps ->
+      let v' t q = if p = q then t else v q in
+      allsatvaluations subfn (v' false) ps @ allsatvaluations subfn (v' true) ps
+
+(* Disjunctive Normal Form
+ *
+ * DNF is computed via truth table enumeration. Worst-case complexity is
+ * O(2^num_atoms) ~= O(2^size_formula) .
+ *)
+let dnf fm =
+  let pvs = atoms fm in
+  let satvals = allsatvaluations (eval fm) (fun _ -> false) pvs in
+  let atoms = List.map (fun p -> Atom p) pvs in
+  list_disj (List.map (mk_lits atoms) satvals)
+
+(* ------------------------------------------------------------------------- *)
 (* Tests                                                                     *)
 (* ------------------------------------------------------------------------- *)
 
@@ -317,32 +404,61 @@ let%test "eliminate logical connectives: {==>, false} are adequate" =
       pp "(p <=> q) <=> ((p ==> q) ==> (q ==> p) ==> false) ==> false";
     ]
 
+let%test "montonicity of and" =
+  tautology (pp "(p ==> p') /\\ (q ==> q') ==> ((p /\\ q) ==> (p' /\\ q'))")
+
+let%test "montonicity of or" =
+  tautology (pp "(p ==> p') /\\ (q ==> q') ==> ((p \\/ q) ==> (p' \\/ q'))")
+
 (* Substitution tests *)
 
 let%test "psubst tautology" =
   tautology (psubst (P "p" |=> pp "p /\\ q") (pp "p /\\ q <=> q /\\ p"))
 
-(* ------------------------------------------------------------------------- *)
-(* Failed attempt at setting up a PPX, in-module, for parsing prop formulas  *)
-(* ------------------------------------------------------------------------- *)
+(* Simplification tests *)
 
-(* TODO: figure out if a PPX can be used here instead of a camlp* preprocessor *)
-(* Set this up as default for quotations.                                    *)
-(* let default_parser = parse_prop_formula *)
-(* #install_printer print_prop_formula *)
+let%expect_test "simplify 1" =
+  prp (psimplify (pp "(true ==> (x <=> false)) ==> ~(y \\/ false /\\ z)"));
+  [%expect {| <<~x ==> ~y>> |}]
 
-(* open Ppxlib *)
-(* open Ppxlib_metaquot *)
-(**)
-(* let expand ~ctxt formula_str = *)
-(*   let loc = Expansion_context.Extension.extension_point_loc ctxt in *)
-(*   Ast_helper.with_default_loc loc (fun () -> *)
-(*       [%expr parse_prop_formula [%e formula_str]]) *)
-(**)
-(* let my_extension = *)
-(*   Extension.V3.declare "pp" Extension.Context.expression *)
-(*     Ast_pattern.(single_expr_payload (estring __)) *)
-(*     expand *)
-(**)
-(* let rule = Ppxlib.Context_free.Rule.extension my_extension *)
-(* let () = Driver.register_transformation ~rules:[ rule ] "pp" *)
+let%expect_test "simplify 2" =
+  prp (psimplify (pp "((x ==> y) ==> true) \\/ ~false"));
+  [%expect {| <<true>> |}]
+
+(* Example of proving a formula is equivalent to its NNF form *)
+
+let%test "fm <=> nnf fm" =
+  let fm = pp "(p <=> q) <=> ~(r ==> s)" in
+  let fm' = nnf fm in
+  tautology (Iff (fm, fm'))
+
+let%expect_test "dnf corresponds to truth table" =
+  let fm = pp "(p \\/ q /\\ r) /\\ (~p \\/ ~r)" in
+  prp (dnf fm);
+  [%expect {| <<~p /\ q /\ r \/ p /\ ~q /\ ~r \/ p /\ q /\ ~r>> |}]
+(* dnf fm = pp "(~p /\\ q /\\ r) \\/ (p /\\ ~q /\\ ~r) \\/ (p /\\ q /\\ ~r)" *)
+
+let%expect_test "truth table of previous formula" =
+  print_truthtable (pp "(p \\/ q /\\ r) /\\ (~p \\/ ~r)");
+  [%expect
+    {|
+    p     q     r     | formula
+    ---------------------------
+    false false false | false
+    false false true  | false
+    false true  false | false
+    false true  true  | true
+    true  false false | true
+    true  false true  | false
+    true  true  false | true
+    true  true  true  | false
+    --------------------------- |}]
+
+let%expect_test "dnf takes a long time" =
+  let fm = pp "(p /\\ q /\\ r /\\ s /\\ t /\\ u) \\/ (u /\\ v)" in
+  prp (dnf fm);
+  [%expect
+    {| <<~p /\ ~q /\ ~r /\ ~s /\ ~t /\ u /\ v \/ ~p /\ ~q /\ ~r /\ ~s /\ t /\ u /\ v \/ ~p /\ ~q /\ ~r /\ s /\ ~t /\ u /\ v \/ ~p /\ ~q /\ ~r /\ s /\ t /\ u /\ v \/ ~p /\ ~q /\ r /\ ~s /\ ~t /\ u /\ v \/ ~p /\ ~q /\ r /\ ~s /\ t /\ u /\ v \/ ~p /\ ~q /\ r /\ s /\ ~t /\ u /\ v \/ ~p /\ ~q /\ r /\ s /\ t /\ u /\ v \/ ~p /\ q /\ ~r /\ ~s /\ ~t /\ u /\ v \/ ~p /\ q /\ ~r /\ ~s /\ t /\ u /\ v \/ ~p /\ q /\ ~r /\ s /\ ~t /\ u /\ v \/ ~p /\ q /\ ~r /\ s /\ t /\ u /\ v \/ ~p /\ q /\ r /\ ~s /\ ~t /\ u /\ v \/ ~p /\ q /\ r /\ ~s /\ t /\ u /\ v \/ ~p /\ q /\ r /\ s /\ ~t /\ u /\ v \/ ~p /\ q /\ r /\ s /\ t /\ u /\ v \/ p /\ ~q /\ ~r /\ ~s /\ ~t /\ u /\ v \/ p /\ ~q /\ ~r /\ ~s /\ t /\ u /\ v \/ p /\ ~q /\ ~r /\ s /\ ~t /\ u /\ v \/ p /\ ~q /\ ~r /\ s /\ t /\ u /\ v \/ p /\ ~q /\ r /\ ~s /\ ~t /\ u /\ v \/ p /\ ~q /\ r /\ ~s /\ t /\ u /\ v \/ p /\ ~q /\ r /\ s /\ ~t /\ u /\ v \/ p /\ ~q /\ r /\ s /\ t /\ u /\ v \/ p /\ q /\ ~r /\ ~s /\ ~t /\ u /\ v \/ p /\ q /\ ~r /\ ~s /\ t /\ u /\ v \/ p /\ q /\ ~r /\ s /\ ~t /\ u /\ v \/ p /\ q /\ ~r /\ s /\ t /\ u /\ v \/ p /\ q /\ r /\ ~s /\ ~t /\ u /\ v \/ p /\ q /\ r /\ ~s /\ t /\ u /\ v \/ p /\ q /\ r /\ s /\ ~t /\ u /\ v \/ p /\ q /\ r /\ s /\ t /\ u /\ ~v \/ p /\ q /\ r /\ s /\ t /\ u /\ v>> |}]
+(* dnf is 1255 bytes *)
+
+(* BIG print_truthtable dnf <<p /\ q /\ r /\ s /\ t /\ u \/ u /\ v>>;; *)
